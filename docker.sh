8ba6e2740aad3a12a26697249b37bef82b542c53
yunus <yunusag10@gmail.com>
2024-05-07 12:41:09 +0000

    modified the installation file

--- docker.sh
+++ docker.sh
@@ -22,5 +22,5 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # add ubuntu user to docker 
-usermod -aG docker ubunut
+usermod -aG docker root
 
